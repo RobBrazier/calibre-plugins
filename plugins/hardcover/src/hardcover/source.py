ab629b93169ae5800e4b0754510a03f3cba212d8
Rob Brazier <git+github@brzr.co>
2024-11-17 19:55:05 +0000

    build(hardcover): manage version via commitzen and native hatch version build hook

--- plugins/hardcover/src/hardcover/source.py
+++ plugins/hardcover/src/hardcover/source.py
@@ -1,14 +1,18 @@
 # pyright: reportIncompatibleMethodOverride=false
 from queue import Empty, Queue
 from calibre.ebooks.metadata.sources.base import Option, Source
-from .__version__ import __version_tuple__
+from .__version__ import __version__
+
+
+def parse_version(version: str) -> tuple[int, ...]:
+    return tuple([int(s) for s in version.split(".")])
 
 
 class Hardcover(Source):
     name = "Hardcover"
     description = "Downloads metadata and covers from Hardcover.app"
     author = "Rob Brazier"
-    version = __version_tuple__
+    version = parse_version(__version__)
     minimum_calibre_version = (7, 7, 0)
 
     ID_NAME = "hardcover"
