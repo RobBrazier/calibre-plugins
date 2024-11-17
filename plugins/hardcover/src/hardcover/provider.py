a5cede755737d2d869ce6672f27cec172c750efa
Rob Brazier <git+github@brzr.co>
2024-11-17 12:31:49 +0000

    move identifier logic into separate testable class

--- plugins/hardcover/src/hardcover/provider.py
+++ plugins/hardcover/src/hardcover/provider.py
@@ -1,20 +1,27 @@
+import queue
 import threading
 from datetime import datetime
 from queue import Queue
-from typing import List, Tuple
+from typing import Optional
 
-from . import queries
+try:
+    from calibre.utils.logging import Log  # noqa: F401
+    from calibre.ebooks.metadata.book.base import Metadata  # noqa: F401
+except ImportError:
+    pass
+except AttributeError:
+    pass
+
 from .models import Book, Edition
+from .identifier import HardcoverIdentifier
 
 
 class HardcoverProvider:
     ID_NAME = "hardcover"
-    API_URL = "https://api.hardcover.app/v1/graphql"
+
+    identifier: Optional[HardcoverIdentifier]
 
     def __init__(self, source):
-        from common.graphql import GraphQLClient
-
-        self.client = GraphQLClient(self.API_URL)
         self.source = source
         self.prefs = source.prefs
 
@@ -30,89 +37,24 @@ class HardcoverProvider:
 
     def identify(
         self,
-        log,
-        result_queue,
-        abort,
-        title=None,
-        authors=None,
+        log,  # type: Log
+        result_queue: queue.Queue,
+        abort: threading.Event,
+        title: Optional[str] = None,
+        authors: Optional[list[str]] = None,
         identifiers={},
         timeout=30,
     ):
-        hardcover_id = identifiers.get(self.ID_NAME, None)
-        hardcover_edition = identifiers.get(f"{self.ID_NAME}-edition", None)
-        isbn = identifiers.get("isbn", "")
-        asin = identifiers.get("mobi-asin", "")
+        identifier = HardcoverIdentifier(
+            log, self.ID_NAME, self.prefs.get("api_key"), timeout
+        )
+        books = identifier.identify(title, authors, identifiers)
 
-        found_exact = False
-        candidate_books = []
-
-        # Exact match with a Hardcover Edition ID
-        if hardcover_edition:
-            books = self.get_book_by_edition(log, hardcover_edition, timeout)
-            if len(books) > 0:
-                candidate_books = books
-                found_exact = True
-
-        # Exact match with a Hardcover ID
-        if hardcover_id and not found_exact:
-            books = self.get_book_by_slug(log, hardcover_id, timeout)
-            if len(books) > 0:
-                candidate_books = books
-                found_exact = True
-
-        # Exact match with an ISBN or ASIN
-        if (isbn or asin) and not found_exact:
-            books = self.get_book_by_isbn_asin(log, isbn, asin, timeout)
-            if len(books) > 0:
-                candidate_books = books
-                found_exact = True
-
-        # Search for an Exact match by Fuzzy Title and Authors
-        if title and authors and not found_exact:
-            books = self.get_book_by_name_authors(log, title, authors, timeout)
-            if len(books) > 0:
-                candidate_books = books
-                found_exact = True
-
-        # Fuzzy Search by Title
-        if title and not found_exact:
-            books = self.get_book_by_name(log, title, timeout)
-
-            candidate_books = books
-
-            # Get closest books by Title
-            candidate_titles: List[Tuple[int, Book]] = []
-            for book in candidate_books:
-                similarity = self.levenshtein_distance(title, book.title)
-                candidate_titles.append((similarity, book))
-            candidate_titles = sorted(candidate_titles, key=lambda x: x[0])
-            if len(candidate_titles) > 20:
-                candidate_titles = candidate_titles[:20]
-            candidate_books = [book[1] for book in candidate_titles]
-
-        for book in candidate_books:
+        for book in books:
             self.enqueue(log, result_queue, abort, book)
         return None
 
-    @staticmethod
-    def levenshtein_distance(s1, s2):
-        if len(s1) > len(s2):
-            s1, s2 = s2, s1
-
-        distances = range(len(s1) + 1)
-        for i2, c2 in enumerate(s2):
-            distances_ = [i2 + 1]
-            for i1, c1 in enumerate(s1):
-                if c1 == c2:
-                    distances_.append(distances[i1])
-                else:
-                    distances_.append(
-                        1 + min((distances[i1], distances[i1 + 1], distances_[-1]))
-                    )
-            distances = distances_
-        return distances[-1]
-
-    def find_matching_edition(self, editions: List[Edition]):
+    def find_matching_edition(self, editions: list[Edition]):
         # TODO: do some actual matching here ...
         if len(editions) > 0:
             return editions[0]
@@ -123,13 +65,14 @@ class HardcoverProvider:
         return Metadata(title, authors)
 
     def build_metadata(self, log, book: Book):
+        # type: (Log, Book) -> Optional[Metadata]
         editions = book.editions
-        matching_edition = self.find_matching_edition(editions)
-        if not matching_edition:
+        if len(editions) == 0:
             log.error("No matching edition")
             return None
-        title = matching_edition.title
-        authors = [c.author.name for c in matching_edition.contributions]
+        edition = editions[0]
+        title = edition.title
+        authors = [c.author.name for c in edition.contributions]
         meta = self.init_metadata(title, authors)
         book_series = book.book_series
         if len(book_series) > 0:
@@ -137,35 +80,33 @@ class HardcoverProvider:
             meta.series = series.series.name
             meta.series_index = series.position  # pyright: ignore
         meta.set_identifier("hardcover", book.slug)
-        meta.set_identifier("hardcover-edition", str(matching_edition.id))
-        if isbn := matching_edition.isbn_13:
+        meta.set_identifier("hardcover-edition", str(edition.id))
+        if isbn := edition.isbn_13:
             meta.set_identifier("isbn", isbn)
             self.source.cache_isbn_to_identifier(isbn, book.slug)
         if book.description:
             meta.comments = book.description
-        if matching_edition.image.url:
+        if edition.image.url:
             meta.has_cover = True
-            self.source.cache_identifier_to_cover_url(
-                book.slug, matching_edition.image.url
-            )
+            self.source.cache_identifier_to_cover_url(book.slug, edition.image.url)
         else:
             meta.has_cover = False
-        if matching_edition.publisher:
-            meta.publisher = matching_edition.publisher.name
-        if language := matching_edition.language.code3:
+        if edition.publisher:
+            meta.publisher = edition.publisher.name
+        if language := edition.language.code3:
             meta.languages = [language]
-        if matching_edition.release_date:
+        if edition.release_date:
             try:
-                meta.pubdate = datetime.strptime(
-                    matching_edition.release_date, "%Y-%m-%d"
-                )
+                meta.pubdate = datetime.strptime(edition.release_date, "%Y-%m-%d")
             except ValueError:
                 log.warn("Unable to parse release date")
         if book.taggings:
+            # you think this contains some tags?
             meta.tags = [tag.tag.tag for tag in book.taggings if tag.tag.tag]
         return meta
 
     def enqueue(self, log, result_queue: Queue, shutdown: threading.Event, book: Book):
+        # type: (Log, Queue, threading.Event, Book) -> None
         if shutdown.is_set():
             raise threading.ThreadError
         metadata = self.build_metadata(log, book)
@@ -173,49 +114,3 @@ class HardcoverProvider:
             result_queue.put(metadata)
             self.source.clean_downloaded_metadata(metadata)
         log.info(f"Adding book slug '{book.slug}' to queue")
-
-    def _execute(self, query, variables=None, timeout=30):
-        if not self.client.token:
-            self.client.set_token(self.prefs["api_key"])
-
-        books = self.client.execute(query, variables, timeout)
-        result: List[Book] = []
-        if "books" not in books:
-            return result
-        for entry in books.get("books", []):
-            result.append(Book.from_dict(entry))
-        return result
-
-    def get_book_by_isbn_asin(
-        self, log, isbn: str, asin: str, timeout=30
-    ) -> list[Book]:
-        log.info("Finding by ISBN / ASIN")
-        query = queries.FIND_BOOK_BY_ISBN_OR_ASIN
-        variables = {"isbn": isbn, "asin": asin}
-        return self._execute(query, variables, timeout)
-
-    def get_book_by_slug(self, log, slug: str, timeout=30) -> list[Book]:
-        log.info("Finding by Slug")
-        query = queries.FIND_BOOK_BY_SLUG
-        variables = {"slug": slug}
-        return self._execute(query, variables, timeout)
-
-    def get_book_by_edition(self, log, edition: str, timeout=30) -> list[Book]:
-        log.info("Finding by Edition ID")
-        query = queries.FIND_BOOK_BY_EDITION
-        variables = {"edition": edition}
-        return self._execute(query, variables, timeout)
-
-    def get_book_by_name(self, log, name: str, timeout=30) -> list[Book]:
-        log.info("Finding by Name")
-        query = queries.FIND_BOOK_BY_NAME
-        variables = {"title": name}
-        return self._execute(query, variables, timeout)
-
-    def get_book_by_name_authors(
-        self, log, name: str, authors: List[str], timeout=30
-    ) -> list[Book]:
-        log.info("Finding by Name + Authors")
-        query = queries.FIND_BOOK_BY_NAME_AND_AUTHORS
-        variables = {"title": name, "authors": authors}
-        return self._execute(query, variables, timeout)
--- plugins/hardcover/src/hardcover/queries.py
+++ plugins/hardcover/src/hardcover/queries.py
@@ -1,3 +1,11 @@
+SEARCH_BY_NAME = """
+query Search($query: String!) {
+  search(query: $query, query_type: "Book", per_page: 50) {
+    results
+  }
+}
+"""
+
 FIND_BOOK_BY_NAME = """
 query FindBookByName($title: String) {
   books(
@@ -186,6 +194,53 @@ FIND_BOOK_BY_EDITION = """
 query FindBookByEdition($edition: Int) {
   books(
     where: {editions: {id: {_eq: $edition}}}
+    order_by: {users_read_count: desc_nulls_last}
+  ) {
+    title
+    slug
+    book_series {
+      series {
+        name
+      }
+    }
+    taggings(where: {spoiler: {_eq: false}}, limit: 10, distinct_on: tag_id) {
+      tag {
+        tag
+      }
+    }
+    editions(
+      where: {_or: [{edition_format: {_neq: "Audiobook"}}, {edition_format: {_is_null: true}}]}
+      order_by: {users_count: desc_nulls_last}
+    ) {
+      id
+      isbn_13
+      title
+      contributions {
+        author {
+          name
+        }
+      }
+      image {
+        url
+      }
+      language {
+        code3
+      }
+      publisher {
+        name
+      }
+      release_date
+    }
+    description
+  }
+}
+"""
+
+
+FIND_BOOKS_BY_IDS = """
+query FindBooksByIds($ids: [Int!]) {
+  books(
+    where: {id: {_in: $ids}}
     order_by: {users_read_count: desc_nulls_last}
   ) {
     title
