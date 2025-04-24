7cf32cca1c75c71adc46b2318e04e68a4da66617
Rob Brazier <git+github@brzr.co>
2025-04-24 17:37:29 +0100

    chore(release): move release stuff into shell script

---
+++ scripts/release.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+tag="$1"
+name="${tag%-*}"
+version="${tag##*-}"
+
+file="RELEASE_NOTES.md"
+
+git cliff --config cliff.toml \
+	--current --include-path "plugins/$name/**/*"  \
+	--tag-pattern "$name-*" --tag "$tag" --output "$file"
+
+contents="$(cat $file)"
+rm "$file"
+
+if [[ "$version" == *"pre"* ]]; then
+	opts="--prerelease"
+else
+	opts="--latest"
+fi
+
+gh release create "$tag" $opts -n "$contents" -t "$tag" **/"$tag"*.zip
